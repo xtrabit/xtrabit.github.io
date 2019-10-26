51f5987411fafc8132a380355462fb4d4c40fa96
Alexei Sorokine <2picae@gmail.com>
2019-10-26 15:22:35 -0700

    Add contact information

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,7 +12,39 @@ export default class Contact extends React.Component {
   render() {
     return (
       <main>
-        contact
+        <section className='contact'>
+          <dl>
+            <dt>name:</dt>
+            <dd>Alexei Sorokine</dd>
+            <dt>phone:</dt>
+            <dd>1 (415) 250-4959</dd>
+            <dt>email</dt>
+            <dd>2picae@gmail.com</dd>
+            <dt>LinkedIn:</dt>
+            <dd>
+              <a href='https://www.linkedin.com/in/alexei-sorokine/' target="_blank">
+                linkedin.com/in/alexei-sorokine/
+              </a>
+            </dd>
+            <dt>GitHub:</dt>
+            <dd>
+              <a href='https://github.com/xtrabit' target="_blank">
+                github.com/xtrabit
+              </a>
+            </dd>
+            <dt>resume</dt>
+            <dd>
+              <a href="/static/Alexei Sorokine Resume.pdf" download>
+                PDF
+              </a>
+            </dd>
+            <dd>
+              <a href="/static/Alexei Sorokine Resume.docx" download>
+                MS Word
+              </a>
+            </dd>
+          </dl>
+        </section>
       </main>
     )
   }
