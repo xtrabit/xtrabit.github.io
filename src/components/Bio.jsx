9d9ec7d363b6a7bed1d84f4124b5e4e7a19f7720
Alexei Sorokine <2picae@gmail.com>
2019-10-26 17:35:37 -0700

    Fix typos

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -13,7 +13,7 @@ export default class Bio extends React.Component {
     return (
       <main>
         <section className='bio'>
-          <h3>Cliffnotes</h3>
+          <h3>Cliffsnotes</h3>
           <p>
             &emsp;I am aware of the difficultly employers face in finding talented candidates
             to fill open positions. I was on the other side of the isle on many occasions, and
@@ -23,7 +23,7 @@ export default class Bio extends React.Component {
             &emsp;As long as I remember, I always liked making things. My first love was
             electricity. I completed my first circuit at four, and found out that
             human body is conductive. Cranes, cars and other things with motors and
-            mooving parts were a part of my daily routine. Started to rebuild engines when I
+            moving parts were a part of my daily routine. Started to rebuild engines when I
             was old enough, and once converted a diesel Mercedes to run on vegetable
             oil. I love to work on complex machines, and when I realized that the
             most complex machine within my reach is an airplane, I joined the Navy.
@@ -34,7 +34,7 @@ export default class Bio extends React.Component {
             to be what I wanted. As I like to be hands on, I stuck around construction
             industry and learned everything I could. Steam bending wood, welding, and
             CNC machining to name a few. When I had to manage others, I learned that
-            there is much fun to be had in solving logistical proplems.
+            there is much fun to be had in solving logistical problems.
           </p>
           <p>
             &emsp;I never lost the draw towards electricity, and at some point decided to
@@ -43,7 +43,7 @@ export default class Bio extends React.Component {
             I discovered an ocean in embedded processors programming. I stayed away
             from premade boards and kits. Calculating loads, heat gains, and routing component
             connections is a thrill for me. As I etched boards and soldered components, I could not
-            restrain myself from driving designs smaller and smaller untill I could no longer work
+            restrain myself from driving designs smaller and smaller until I could no longer work
             without 5X magnifying glass.
           </p>
           <p>
@@ -55,7 +55,7 @@ export default class Bio extends React.Component {
             through web development.
           </p>
           <p>
-            &emsp;I would not claim to be talented, and like all humans, have my shortcumings. I like
+            &emsp;I would not claim to be talented, and like all humans, have my shortcomings. I like
             to get sidetracked, and enjoy personal projects because they lack time pressure, apart from
             the one I set for myself. I like to get lost in details and then find myself again in them.
             Despite my experience, I still get afraid when appointed lead on a project that is new
